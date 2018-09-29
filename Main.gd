771a9a426a111348ea0a4a1bb5ebfe4907f81857
arnav dhamija <arnav.dhamija@gmail.com>
2018-09-30 04:33:29 +0530

    Reset scene

--- Main.gd
+++ Main.gd
@@ -5,7 +5,6 @@ var current_level_node
 const cat_scene = preload("res://objs/Cat.tscn")
 var cat = cat_scene.instance()
 var start_pos = Vector2(0,0)
-var cat_ref = weakref(cat)
 
 func init_cat():
 	cat.position = start_pos
@@ -14,7 +13,7 @@ func init_cat():
 func load_level(level_num):
 	if level_num == 0:
 		current_level_scene = preload("res://levels/level0.tscn")
-		start_pos = Vector2(256,200)
+		start_pos = Vector2(1000,200)
 	current_level_node = current_level_scene.instance()
 	add_child(current_level_node)
 
@@ -23,10 +22,5 @@ func _ready():
 	init_cat()
 
 func _process(delta):
-	if cat_ref.get_ref() == null:
-		cat = cat_scene.instance()
-		init_cat()
-		cat_ref = weakref(cat)
-
-#	if cat.position.y > get_viewport().get_visible_rect().end.y:
-#		cat.queue_free()
+	if cat.position.y > get_viewport().get_visible_rect().end.y:
+		get_tree().reload_current_scene()
