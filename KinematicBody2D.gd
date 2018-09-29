c4618902ce349b6609dec9d97f1939e660c9b8d8
TestSubjector <prasunk3@gmail.com>
2018-09-29 10:47:07 +0530

    Added cat colour switch

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
-onready var cat_sprite = get_node("cat_sprite")
+onready var cat_sprite = get_node("whitecat_sprite")
+
 
 # class member variables go here, for example:
 # var a = 2
@@ -11,7 +12,7 @@ const GRAVITY  = 20
 const MOV_MOTION = 300
 const JUMP_HEIGHT = -400
 
-
+var cat_status = 0 
 var anim = "idle"
 
 var motion = Vector2()
@@ -19,7 +20,17 @@ var motion = Vector2()
 func _physics_process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-
+	
+	if(cat_status == 1):
+		get_node("whitecat_sprite").hide()
+		get_node("blackcat_sprite").show()
+		cat_sprite = get_node("blackcat_sprite")
+	elif(cat_status == 0):
+			get_node("blackcat_sprite").hide()
+			get_node("whitecat_sprite").show()
+			cat_sprite = get_node("whitecat_sprite")
+	
+	
 	# Gravity
 	motion.y += GRAVITY # So every frame the gravity effect is included
 
@@ -29,6 +40,13 @@ func _physics_process(delta):
 		motion.x = -MOV_MOTION
 	else:
 		motion.x = 0
+	
+	if Input.is_key_pressed(65):
+		if(cat_status == 0):
+			cat_status = 1
+		elif(cat_status == 1):
+			cat_status = 0
+	
 	
 	if is_on_floor():
 		# print("On floor") # For debugging
