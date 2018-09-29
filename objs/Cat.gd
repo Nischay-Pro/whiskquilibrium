bfade147181fd34596d0fc136e69d288a0eb0e2a
Kushal Agrawal <kushalagrawal17@gmail.com>
2018-09-30 00:44:43 +0530

    Add floating mechanism to white cat

--- objs/Cat.gd
+++ objs/Cat.gd
@@ -13,6 +13,8 @@ const WHITE_CAT = 1
 var cat_status = BLACK_CAT
 var anim = "idle"
 
+var float_count = 20
+
 var motion = Vector2()
 
 func basic_physics(delta):	
@@ -25,8 +27,6 @@ func basic_physics(delta):
 	inactive_cat_sprite.hide()
 	active_cat_sprite.show()
 	
-	motion.y += GRAVITY # So every frame the gravity effect is included
-
 	if Input.is_action_pressed("ui_right"):
 		motion.x = MOV_MOTION
 	elif Input.is_action_pressed("ui_left"):
@@ -56,16 +56,17 @@ func black_cat_physics(delta):
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up") or Input.is_key_pressed(32):
 			motion.y = JUMP_HEIGHT
+	motion.y += GRAVITY
 
 func white_cat_physics(delta):
-	pass
+	motion.y = 0
 
 func _physics_process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-	basic_physics(delta)
 	if cat_status == BLACK_CAT:
 		black_cat_physics(delta)
 	elif cat_status == WHITE_CAT:
 		white_cat_physics(delta)
+	basic_physics(delta)
 
