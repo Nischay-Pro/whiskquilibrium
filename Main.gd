5da526380299b32bf7744f7ab244e6461c109bc2
arnav dhamija <arnav.dhamija@gmail.com>
2018-09-30 07:13:53 +0530

    Cleanup

--- Main.gd
+++ Main.gd
@@ -1,37 +1,39 @@
 extends Node
+const cat_scene = preload("res://objs/Cat.tscn")
 
 var curr_level = -1
-var current_level_scene
-var current_level_node
-const cat_scene = preload("res://objs/Cat.tscn")
-var cat = cat_scene.instance()
+var curr_lvl_obj
+var cat
 var start_pos = Vector2(0,0)
 
 func init_cat():
+	cat = cat_scene.instance()
 	cat.position = start_pos
 	add_child(cat)
 
+func init_level(curr_level_scene):
+	curr_lvl_obj = curr_level_scene.instance()
+	add_child(curr_lvl_obj)
+	cat.position = start_pos
+
 func load_level():
-	print("LOADED")
-#	print(get_tree().get_root().get_children())
 	var level = get_node("level_test")
-	get_tree().get_root().remove_child(level)
+#	var arr = get_tree().get_root().get_node("Main").get_children()
+#	for i in arr:
+#		print(i.get_name())
+	get_tree().get_root().remove_child(curr_lvl_obj)
 	level.queue_free()
 	var level_num = curr_level + 1
 	if level_num == 0:
 		start_pos = Vector2(256,200)
-		init_cat()
-		current_level_scene = preload("res://levels/level0.tscn")
-		add_child(current_level_scene.instance())
+		init_level(preload("res://levels/level0.tscn"))
 
-#		get_tree().change_scene("res://levels/level0.tscn")
-#	current_level_node = current_level_scene.instance()
-#	add_child(current_level_node)
 
 func _ready():
-#	load_level(0)
-	current_level_scene = preload("res://levels/level_test.tscn")
-	add_child(current_level_scene.instance())
+	# start with level_test
+	var curr_level_scene = preload("res://levels/level_test.tscn")
+	curr_lvl_obj = curr_level_scene.instance()
+	add_child(curr_lvl_obj)
 	init_cat()
 
 func _process(delta):
--- objs/black_tile.gd
+++ objs/black_tile.gd
@@ -7,12 +7,8 @@ var tile_state = BLACK
 var cat_node = null
 
 func _ready():
-	print("creating blacknodes")
 	get_node("BlackSprite").show()
 	get_node("WhiteSprite").hide()
-	var arr = get_tree().get_root().get_node("Main").get_children()
-	for i in arr:
-		print(i.get_name())
 	cat_node = get_tree().get_root().get_node("Main").get_node("Cat")
 	
 func flip_color():
